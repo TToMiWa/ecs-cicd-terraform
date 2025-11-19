9aa52b7341f4d2e54848b12e885162cf97e81f87
tomiwa <aturutommy@gmail.com>
2025-11-19 17:48:15 +0100

    new change in the browser display

--- notepad-app/notepad1app.js
+++ notepad-app/notepad1app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("new cicd implementation works well! 🚀");
+  res.send("new cicd Implementation works well! 🚀");
 });
 
 app.get("/health", (req, res) => {
