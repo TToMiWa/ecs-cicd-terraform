3973891a787beaf05ba76868dd7a31188589b865
tomiwa <aturutommy@gmail.com>
2025-11-19 15:25:41 +0100

    i changed the display message on the browser

--- notepad-app/notepad1app.js
+++ notepad-app/notepad1app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Terraform! 🚀");
+  res.send("new cicd implementation works well! 🚀");
 });
 
 app.get("/health", (req, res) => {
